beded9c63ab4748cf59029395bfd6d73bfd65538
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-05-16 21:37:08 +0600

    change loading

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -6,7 +6,7 @@ const Loading = () => {
             <div className="flex items-center justify-center h-screen">
     <div className="relative">
         <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-        <div className=" top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
+        <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
         </div>
     </div>
 </div>
